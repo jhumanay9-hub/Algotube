95a23b0dc97b1a11d3216f5e257a56e59c39d015
Jake Humanay <jhumanay9@gmail.com>
2026-03-24 10:50:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3409312941-51d29",
+  "appId": "1:1077395103154:web:6e33905e6221bd034b18ab",
+  "apiKey": "AIzaSyBTBnxuI9H0wqHCV1a3N30IHXskgF7q55M",
+  "authDomain": "studio-3409312941-51d29.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1077395103154"
+};
